79b8ad676d46a091f4fb7d2ca9356c822f285206
ishimwe5555 <ishimwe96@gmail.com>
2020-01-19 08:46:22 +0200

    Fixed issues with footer of admin dashboard

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,7 +3,7 @@ function check(form){
  // var user=document.getElementById('username');
   //var pw=document.getElementById('pw');
   if (form.username.value=="admin" && form.password.value=="admin"){
-window.location="../admin/admin-view.html";
+window.location="../admin/home.html";
   }
   else{
  window.location="home.html";
